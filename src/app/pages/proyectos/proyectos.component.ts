8a246796c05579c6806fe78eae0d215c5375f048
yersonrincon <yersonhernandez202@gmail.com>
2023-05-17 16:15:22 -0500

    cuarto comit

--- src/app/pages/proyectos/proyectos.component.ts
+++ src/app/pages/proyectos/proyectos.component.ts
@@ -13,17 +13,17 @@ import {register} from 'swiper/element/bundle';
 export class ProyectosComponent implements OnInit {
     members: {title: string, descripcion: string, content: string , src: string ,url:string}[] = [
     {title: 'La Pesada', url:'https://github.com/yersonrincon/front-lapesada', content: 'Content here',src:'..//../assets/img/Captura.PNG',
-     descripcion: 'Es un sistema de inventario para almacenes de repuestos de maquinaria pesada desarrollado en ANGULAR NODE JS con base de datos en POSGREST.'},
+     descripcion: 'Sistema de inventario para almacenes  de maquinaria pesada desarrollado en Angular  Node.js con base de datos en PostgreSQL.'},
     {title: 'Spotyapp', url:'https://github.com/yersonrincon/spoty-app',content: 'Content here',src:'..//../assets/img/Captura4.PNG',
-     descripcion: 'Es una aplicación que consume un servicio de Spotify donde nos trae información de lo más escuchado por los usuarios en la app.'},
+     descripcion: 'Aplicación que consume una API de Spotify donde nos trae información de lo más escuchado por los usuarios.'},
     {title: 'Adminpro', url:'https://github.com/yersonrincon/adminpro-front', content: 'Content here',src:'..//../assets/img/Captura6.PNG',
-     descripcion: 'Sistema de registro de médicos y hospitales con gráficas de notificaciones  desarrollado en ANGULAR NODE JS y base de datos en MONGODB.'},
+     descripcion: 'Sistema de registro de médicos y hospitales con gráficas de notificaciones desarrollado en Angular Node.js y base de datos en MongoDB.'},
     {title: 'Tifon',  url:'https://github.com/yersonrincon/tifon-front', content: 'Content here',src:'..//../assets/img/Captura1.PNG',
-    descripcion: 'Sistema de contror de ventas para lavaderos de autos nos  muestra en tiempo real las ventas que se an realizado en el dia. '},
+    descripcion: 'Sistema de control de ventas, para Autolavados, nos  que muestra las ventas en tiempo real.'},
     {title: 'Peliculas',  url:'https://github.com/yersonrincon/peliculas-app',content: 'Content here',src:'..//../assets/img/Captura8.PNG',
     descripcion: 'Aplicacion que consume la información de una app de peliciulas nos nuestrar las peliculas mas vistas con su calificación.' },
     {title: 'Formulario',  url:'https://github.com/yersonrincon/formularioangular', content: 'Content here',src:'..//../assets/img/Captura9.PNG',
-    descripcion: 'Es un formulario que nos permite almacenar datos en el local storage  tiene cierto tipo de validaciones a la hora de ingresar datos.'},
+    descripcion: 'Formulario que nos permite almacenar datos en el local Storage tiene cierto tipo de validaciones  a la hora de ingresar datos.'},
    
   ];
 
