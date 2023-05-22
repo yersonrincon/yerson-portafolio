d0d19b15269582f5f5b10a5ea9dbc42339f87a0d
yersonrincon <yersonhernandez202@gmail.com>
2023-05-22 11:20:15 -0500

    first commit

--- src/app/pages/proyectos/proyectos.component.ts
+++ src/app/pages/proyectos/proyectos.component.ts
@@ -15,7 +15,7 @@ export class ProyectosComponent implements OnInit {
     {title: 'Adminpro', url:'https://github.com/yersonrincon/adminpro-front', content: 'Content here',src:'..//../assets/img/Captura6.PNG',
      descripcion: 'Sistema de registro de médicos y hospitales con gráficas de notificaciones desarrollado en Angular Node.js y base de datos en MongoDB.'},
     {title: 'Tifon',  url:'https://github.com/yersonrincon/tifon-front', content: 'Content here',src:'..//../assets/img/Captura1.PNG',
-    descripcion: 'Sistema de control de ventas, para Autolavados, nos  que muestra las ventas en tiempo real.'},
+    descripcion: 'Sistema de control de ventas, para Autolavados, nos  muestra las ventas en tiempo real.'},
     {title: 'Peliculas',  url:'https://github.com/yersonrincon/peliculas-app',content: 'Content here',src:'..//../assets/img/Captura8.PNG',
     descripcion: 'Aplicación que consume una API  de una App de películas nos  trae información de las películas más vistas  por los usuarios con su calificación.' },
     {title: 'Formulario',  url:'https://github.com/yersonrincon/formularioangular', content: 'Content here',src:'..//../assets/img/Captura9.PNG',
