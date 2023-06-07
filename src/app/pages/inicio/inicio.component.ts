0a6fbbb18b65975b16b33cc464842593ed6fd8f4
yersonrincon <yersonhernandez202@gmail.com>
2023-06-07 11:10:43 -0500

    first commit

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -105,7 +105,7 @@ export class InicioComponent implements OnInit {
           sync: false
         }
       },
-      number: { density: { enable: true, area: 800 }, value: 6 },
+      number: { density: { enable: true, area: 800 }, value: 4 },
       opacity: {
         animation: { enable: true, minimumValue: 0.5, speed: 1, sync: false },
         random: false,
